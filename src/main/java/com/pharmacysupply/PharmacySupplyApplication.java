536262dc0724323be596398cfac81fbd8e5f039d
VaibhavMarwah <vaibhavmarwah1998@gmail.com>
2022-07-11 16:32:40 +0530

    Initial commit

---
+++ src/main/java/com/pharmacysupply/PharmacySupplyApplication.java
@@ -0,0 +1,25 @@
+package com.pharmacysupply;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+
+
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableSwagger2
+public class PharmacySupplyApplication {
+	public static void main(String[] args) {
+	
+		SpringApplication.run(PharmacySupplyApplication.class, args);
+	
+	}
+
+}
